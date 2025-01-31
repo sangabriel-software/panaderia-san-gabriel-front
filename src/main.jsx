1c27834af173605e81ec5524d51f7a76b5a48ac1
aagarcia-se <angelgarciap.dev@gmail.com>
2025-01-31 12:38:17 -0600

    Modificar versiones

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary.jsx";
 import App from "./App.jsx";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -11,7 +13,9 @@ createRoot(document.getElementById("root")).render(
         v7_relativeSplatPath: true, // Cambia la resolución de rutas relativas en rutas splat (*).
       }}
     >
-      <App />
+      <ErrorBoundary> {/* Envuelve tu App con el Error Boundary */}
+        <App />
+      </ErrorBoundary>
     </BrowserRouter>
   </StrictMode>
 );
