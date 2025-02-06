a68c394720492b4abc2f00b6cdc009f72ad22ef5
aagarcia-se <angelgarciap.dev@gmail.com>
2025-02-05 22:03:32 -0600

    Fix navbar todos los dispositivos

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,16 +6,35 @@ import UserDropdown from '../UserDropdown/UserDropdown';
 import { useMediaQuery } from 'react-responsive';
 
 function NavigationBar({ onMenuClick }) {
-  const isSmallScreen = useMediaQuery({ query: '(max-width: 768px)' });
+  
+  // Consulta para determinar si el ancho máximo es de 360px
+  const isMax360 = useMediaQuery({ query: '(max-width: 360px)' });
+  // Consulta para determinar si el ancho máximo es de 768px
+  const isMax768 = useMediaQuery({ query: '(max-width: 768px)' });
+
+  // Definimos un tamaño de fuente base para pantallas grandes
+  let fontSize = '20px';
+
+  if (isMax360) {
+    fontSize = '17px';
+  } else if (isMax768) {
+    fontSize = '16px';
+  }
 
   return (
-    <Navbar className='topbar' bg="danger" variant="dark" expand="lg" fixed="top">
+    <Navbar className="topbar" bg="danger" variant="dark" expand="lg" fixed="top">
       <Container fluid>
         <div className="d-flex align-items-center w-100">
           <MenuToggle onMenuClick={onMenuClick} />
-          <Navbar.Brand href="/dashboard" className="me-auto ms-2 text-truncate" style={{ maxWidth: '200px', fontSize: isSmallScreen ? '14px' : '16px' }}>
+          
+          <Navbar.Brand
+            href="/dashboard"
+            className="me-auto ms-2 text-truncate"
+            style={{ maxWidth: '200px', fontSize }}
+          >
             Panaderia San Gabriel
           </Navbar.Brand>
+
           <div className="d-flex align-items-center">
             <NotificationBadge />
             <UserDropdown />
