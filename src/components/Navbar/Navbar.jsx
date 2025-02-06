5a85a64bb53d2e29daede763a3a9d13a8350438d
aagarcia-se <angelgarciap.dev@gmail.com>
2025-02-05 21:51:22 -0600

    Fix navbar todos los dispositivos

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,20 +3,23 @@ import { Navbar, Container } from 'react-bootstrap';
 import MenuToggle from '../MenuToogle/MenuToogle';
 import NotificationBadge from '../NotificationBadage/NotificationBadge';
 import UserDropdown from '../UserDropdown/UserDropdown';
-
+import { useMediaQuery } from 'react-responsive';
 
 function NavigationBar({ onMenuClick }) {
+  const isSmallScreen = useMediaQuery({ query: '(max-width: 768px)' });
+
   return (
-    <Navbar className='topbar' bg="danger" variant="dark" expand={false} fixed="top">
+    <Navbar className='topbar' bg="danger" variant="dark" expand="lg" fixed="top">
       <Container fluid>
-        <div className="d-flex align-items-center">
+        <div className="d-flex align-items-center w-100">
           <MenuToggle onMenuClick={onMenuClick} />
-          <Navbar.Brand href="/dashboard">Panaderia San Gabriel</Navbar.Brand>
-        </div>
-        
-        <div className="d-flex align-items-center">
-          <NotificationBadge />
-          <UserDropdown />
+          <Navbar.Brand href="/dashboard" className="me-auto ms-2 text-truncate" style={{ maxWidth: '200px', fontSize: isSmallScreen ? '14px' : '16px' }}>
+            Panaderia San Gabriel
+          </Navbar.Brand>
+          <div className="d-flex align-items-center">
+            <NotificationBadge />
+            <UserDropdown />
+          </div>
         </div>
       </Container>
     </Navbar>
