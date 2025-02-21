563c8fd310f99bcb4354208deda9a6a9dac2f0f0
aagarcia-se <angelgarciap.dev@gmail.com>
2025-02-21 17:40:59 -0600

    fix drowpdown topbar background

--- src/components/UserDropdown/UserDropdown.jsx
+++ src/components/UserDropdown/UserDropdown.jsx
@@ -8,13 +8,13 @@ function UserDropdown() {
 
   return (
     <Dropdown align="end">
-      <Dropdown.Toggle variant="link" className="nav-link p-0 text-light">
+      <Dropdown.Toggle variant="link" className="nav-link p-0 text-light custom-dropdown-toggle">
         <Image
           src="https://i.pinimg.com/originals/d3/a2/9d/d3a29d4e0fa8e004b274880ec979b1e2.jpg"
           roundedCircle
           width={40}
           height={40}
-          className="border border-2 border-light"
+          className="border border-2 border-light custom-image-dropdown"
         />
       </Dropdown.Toggle>
 
