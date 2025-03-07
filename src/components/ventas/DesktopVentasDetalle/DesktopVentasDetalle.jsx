c277f53bd8c52d7c75f9edcd74616c102ebe176c
aagarcia-se <angelgarciap.dev@gmail.com>
2025-03-07 15:14:31 -0600

    Agregar ux y ui para detalle de la venta, vista desktop

--- src/components/ventas/DesktopVentasDetalle/DesktopVentasDetalle.jsx
+++ src/components/ventas/DesktopVentasDetalle/DesktopVentasDetalle.jsx
@@ -3,6 +3,17 @@ import { Badge, Card, Col, Container, Row, Table } from "react-bootstrap";
 import DownloadDropdown from "../../../components/DownloadDropdown/DownloadDropdown";
 import { formatDateToDisplay } from "../../../utils/dateUtils";
 import "./DesktopVentasDetalle.css";
+import {
+  BsCash,
+  BsDashCircle,
+  BsWallet,
+  BsPerson,
+  BsShop,
+  BsCalendar,
+  BsClock,
+  BsCheckCircle,
+  BsXCircle,
+} from "react-icons/bs";
 
 const DesktopVentaDetalle = ({ venta, onDownloadXLS, onDownloadPDF }) => {
   // Acceder a los datos anidados
@@ -12,111 +23,103 @@ const DesktopVentaDetalle = ({ venta, onDownloadXLS, onDownloadPDF }) => {
 
   return (
     <Container fluid>
+      {/* Encabezado Unificado */}
       <DesktopHeader
         encabezadoVenta={encabezadoVenta}
-        detalleIngresos={detalleIngresos}
         onDownloadXLS={onDownloadXLS}
         onDownloadPDF={onDownloadPDF}
       />
+
+      {/* Tabla de Productos */}
       <OrderTable productos={detallesVenta} />
+
+      {/* Balance con Estilo Mejorado */}
+      <Balance detalleIngresos={detalleIngresos} />
     </Container>
   );
 };
 
-const DesktopHeader = ({ encabezadoVenta, detalleIngresos, onDownloadXLS, onDownloadPDF }) => {
+const DesktopHeader = ({ encabezadoVenta, onDownloadXLS, onDownloadPDF }) => {
   // Función para formatear números con dos decimales y símbolo Q
   const formatCurrency = (value) => {
     return `Q ${parseFloat(value).toFixed(2)}`;
   };
 
-  // Determinar el color de la diferencia
-  const diferenciaColor = detalleIngresos?.diferencia >= 0 ? "success" : "danger";
-
   return (
-    <Card
-      className="shadow-lg border-0 mb-4 bg-gradient-primary"
-      style={{ borderRadius: "15px" }}
-    >
+    <Card className="shadow-lg border-0 mb-4 bg-gradient-primary" style={{ borderRadius: "15px" }}>
       <Card.Body className="p-4">
         <Row className="g-3">
-          <Col md={4} className="border-end border-light">
-            <h3 className="mb-3 fw-bold">{`VENTA #${encabezadoVenta?.idVenta}`}</h3>
+          {/* Información de la Venta */}
+          <Col md={4}>
             <div className="d-flex flex-column text-dark">
+              <h3 className="mb-3 fw-bold">{`VENTA #${encabezadoVenta?.idVenta}`}</h3>
               <div className="mb-3">
-                <span className="text-uppercase small opacity-75">
-                  Fecha de venta
+                <span className="text-uppercase small opacity-75 d-flex align-items-center gap-2">
+                  <BsPerson size={16} /> Usuario que ingresó la venta
                 </span>
-                <h3 className="mb-0 fw-bold">
-                  {formatDateToDisplay(encabezadoVenta?.fechaVenta)}
-                </h3>
+                <h5 className="mb-0 fw-semibold">{`@${encabezadoVenta?.usuario}`}</h5>
               </div>
-              <div>
-                <span className="text-uppercase small opacity-75">Sucursal</span>
-                <h4 className="mb-0 fw-semibold">{encabezadoVenta?.nombreSucursal}</h4>
-              </div>
-              <div>
-                <span className="text-uppercase small opacity-75">Usuario</span>
-                <h4 className="mb-0 fw-semibold">{encabezadoVenta?.usuario}</h4>
+              <div className="mb-3">
+                <span className="text-uppercase small opacity-75 d-flex align-items-center gap-2">
+                  <BsShop size={16} /> Sucursal
+                </span>
+                <h5 className="mb-0 fw-semibold">{encabezadoVenta?.nombreSucursal}</h5>
               </div>
             </div>
           </Col>
-          <Col md={3} className="border-end border-light">
+
+          {/* Fecha y Turno */}
+          <Col md={4}>
             <div className="d-flex flex-column text-dark">
               <div className="mb-3">
-                <span className="text-uppercase small opacity-75">Turno</span>
-                <h3 className="mb-0 fw-bold">{encabezadoVenta?.ordenTurno || "N/A"}</h3>
+                <span className="text-uppercase small opacity-75 d-flex align-items-center gap-2">
+                  <BsCalendar size={16} /> Fecha Venta
+                </span>
+                <h5 className="mb-0 fw-bold">
+                  {formatDateToDisplay(encabezadoVenta?.fechaVenta)}
+                </h5>
               </div>
+              <div>
+                <span className="text-uppercase small opacity-75 d-flex align-items-center gap-2">
+                  <BsClock size={16} /> Turno
+                </span>
+                <h5 className="mb-0 fw-bold">{encabezadoVenta?.ordenTurno || "N/A"}</h5>
+              </div>
+            </div>
+          </Col>
+
+          {/* Estado, Venta Ingresada y Botón de Descargar */}
+          <Col md={4}>
+            <div className="d-flex flex-column text-dark h-100 justify-content-between">
               <div className="mb-3">
-                <span className="text-uppercase small opacity-75">Estado</span>
-                <h3 className="mb-0 fw-bold">
+                <span className="text-uppercase small opacity-75 d-flex align-items-center gap-2">
+                  {encabezadoVenta?.estadoVenta === "C" ? (
+                    <BsCheckCircle size={16} className="text-success" />
+                  ) : (
+                    <BsXCircle size={16} className="text-danger" />
+                  )}{" "}
+                  Estado Venta
+                </span>
+                <h5 className="mb-0 fw-bold">
                   <Badge
                     bg={encabezadoVenta?.estadoVenta === "C" ? "success" : "danger"}
                     className="px-1 py-1"
                   >
                     {encabezadoVenta?.estadoVenta === "C" ? "Cerrada" : "Pendiente"}
                   </Badge>
-                </h3>
+                </h5>
               </div>
               <div>
-                <span className="text-uppercase small opacity-75">
-                  Venta Ingresada
+                <span className="text-uppercase small opacity-75 d-flex align-items-center gap-2">
+                  <BsCash size={16} /> Venta Ingresada
                 </span>
-                <h4 className="mb-0 fw-semibold">
-                  {formatCurrency(encabezadoVenta?.totalVenta)}
-                </h4>
+                <h5 className="mb-0 fw-semibold mb-2">
+                  <Badge bg="primary" className="px-2 py-1">
+                    {formatCurrency(encabezadoVenta?.totalVenta)}
+                  </Badge>
+                </h5>
               </div>
-            </div>
-          </Col>
-          <Col md={5} className="ps-5">
-            <div className="d-flex flex-column text-dark">
-              <div className="mb-3">
-                <span className="text-uppercase small opacity-75">
-                  Balance
-                </span>
-                <div className="d-flex flex-column gap-2 mt-2">
-                  <div>
-                    <span className="fw-medium">Monto Esperado: </span>
-                    <span className="fw-bold">
-                      {formatCurrency(detalleIngresos?.montoEsperado)}
-                    </span>
-                  </div>
-                  <div>
-                    <span className="fw-medium">Monto Ingresado: </span>
-                    <span className="fw-bold">
-                      {formatCurrency(detalleIngresos?.montoTotalIngresado)}
-                    </span>
-                  </div>
-                  <div>
-                    <span className="fw-medium">Diferencia: </span>
-                    <span className="fw-bold">
-                      <Badge bg={diferenciaColor} className="px-2 py-1">
-                        {formatCurrency(detalleIngresos?.diferencia)}
-                      </Badge>
-                    </span>
-                  </div>
-                </div>
-              </div>
-              <div className="mt-auto">
+              <div className="mt-auto mt-5">
                 <DownloadDropdown
                   onDownloadXLS={onDownloadXLS}
                   onDownloadPDF={onDownloadPDF}
@@ -144,10 +147,7 @@ const OrderTable = ({ productos }) => {
   }, 0);
 
   return (
-    <Card
-      className="shadow-lg border-0 overflow-hidden mb-5"
-      style={{ borderRadius: "15px" }}
-    >
+    <Card className="shadow-lg border-0 overflow-hidden mb-4" style={{ borderRadius: "15px" }}>
       <div className="table-responsive">
         <Table hover className="mb-0">
           <thead className="bg-dark text-white">
@@ -182,6 +182,60 @@ const OrderTable = ({ productos }) => {
           </tbody>
         </Table>
       </div>
+    </Card>
+  );
+};
+
+const Balance = ({ detalleIngresos }) => {
+  // Función para formatear números con dos decimales y símbolo Q
+  const formatCurrency = (value) => {
+    return `Q ${parseFloat(value).toFixed(2)}`;
+  };
+
+  // Determinar el color de la diferencia
+  const diferenciaColor = detalleIngresos?.diferencia >= 0 ? "success" : "danger";
+
+  return (
+    <Card className="shadow-lg border-0 mb-4 bg-gradient-primary" style={{ borderRadius: "15px" }}>
+      <Card.Body className="p-4">
+        <Row className="g-3">
+          <Col md={12}>
+            <div className="d-flex flex-column text-dark">
+              <h3 className="mb-3 fw-bold">Balance</h3>
+              <div className="d-flex flex-column gap-3">
+                {/* Monto Esperado */}
+                <div className="d-flex align-items-center gap-2">
+                  <BsCash size={20} className="text-primary" />
+                  <span className="fw-medium">Monto Esperado: </span>
+                  <span className="fw-bold">
+                    {formatCurrency(detalleIngresos?.montoEsperado)}
+                  </span>
+                </div>
+
+                {/* Monto Ingresado */}
+                <div className="d-flex align-items-center gap-2">
+                  <BsWallet size={20} className="text-primary" />
+                  <span className="fw-medium">Monto Ingresado: </span>
+                  <span className="fw-bold">
+                    {formatCurrency(detalleIngresos?.montoTotalIngresado)}
+                  </span>
+                </div>
+
+                {/* Operación Matemática */}
+                <div className="d-flex align-items-center gap-2">
+                  <BsDashCircle size={20} className="text-danger" />
+                  <span className="fw-medium">Diferencia: </span>
+                  <span className="fw-bold">
+                    <Badge bg={diferenciaColor} className="px-2 py-1">
+                      {formatCurrency(detalleIngresos?.diferencia)}
+                    </Badge>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Card.Body>
     </Card>
   );
 };
