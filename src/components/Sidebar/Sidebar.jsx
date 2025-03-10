b88d7411524426cb85c874e4f00efee181dfd18f
aagarcia-se <angelgarciap.dev@gmail.com>
2025-03-10 15:58:36 -0600

    agergar servivio para ingreso de configuraciones

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -21,6 +21,7 @@ import { MdDashboard, MdOutlineBakeryDining } from "react-icons/md";
 import * as DarkReader from "darkreader";
 import "./Sidebar.css";
 import { getUserData, getUserPermissions } from "../../utils/Auth/decodedata";
+import { getColorFromName } from "./Sidebar.uitils";
 
 function Sidebar({ show, onClose }) {
   const [usersOpen, setUsersOpen] = useState(false);
@@ -41,63 +42,7 @@ function Sidebar({ show, onClose }) {
     return permissionsMap[route];
   };
 
-  // Función para generar un color basado en el nombre del usuario
-  const getColorFromName = (name) => {
-    const colors = [
-      "#FF6633",
-      "#FFB399",
-      "#FF33FF",
-      "#FFFF99",
-      "#00B3E6",
-      "#E6B333",
-      "#3366E6",
-      "#999966",
-      "#99FF99",
-      "#B34D4D",
-      "#80B300",
-      "#809900",
-      "#E6B3B3",
-      "#6680B3",
-      "#66991A",
-      "#FF99E6",
-      "#CCFF1A",
-      "#FF1A66",
-      "#E6331A",
-      "#33FFCC",
-      "#66994D",
-      "#B366CC",
-      "#4D8000",
-      "#B33300",
-      "#CC80CC",
-      "#66664D",
-      "#991AFF",
-      "#E666FF",
-      "#4DB3FF",
-      "#1AB399",
-      "#E666B3",
-      "#33991A",
-      "#CC9999",
-      "#B3B31A",
-      "#00E680",
-      "#4D8066",
-      "#809980",
-      "#E6FF80",
-      "#1AFF33",
-      "#999933",
-      "#FF3380",
-      "#CCCC00",
-      "#66E64D",
-      "#4D80CC",
-      "#9900B3",
-      "#E64D66",
-      "#4DB380",
-      "#FF4D4D",
-      "#99E6E6",
-      "#6666FF",
-    ];
-    const hash = name.split("").reduce((acc, char) => char.charCodeAt(0) + acc, 0);
-    return colors[hash % colors.length];
-  };
+
 
   // Efecto para aplicar el tema al cargar el componente
   useEffect(() => {
@@ -316,6 +261,20 @@ function Sidebar({ show, onClose }) {
               />
             </Nav.Link>
 
+            <Collapse in={configOpen}>
+              <div>
+                {isRouteAllowed("/config/control-panel") && (
+                  <Nav.Link
+                    as={NavLink}
+                    to="/config/control-panel"
+                    className="text-light ps-4 submenu-item"
+                    onClick={handleNavLinkClick}
+                  >
+                    <MdDashboard className="me-2" /> Panel de control
+                  </Nav.Link>
+                )}
+              </div>
+            </Collapse>
             <Collapse in={configOpen}>
               <div>
                 {isRouteAllowed("/config/control-panel") && (
