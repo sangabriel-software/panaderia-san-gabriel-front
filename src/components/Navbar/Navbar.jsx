e580b460a194dc468ed9c8e187a06db985b35ec2
aagarcia-se <angelgarciap.dev@gmail.com>
2025-03-17 22:46:30 -0600

    ux y ui para sucursales

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
-import MenuToggle from '../MenuToogle/MenuToogle';
-import NotificationBadge from '../NotificationBadage/NotificationBadge';
-import UserDropdown from '../UserDropdown/UserDropdown';
+import React from "react";
+import { Navbar, Container } from "react-bootstrap";
+import MenuToggle from "../MenuToogle/MenuToogle";
+import NotificationBadge from "../NotificationBadage/NotificationBadge";
+import UserDropdown from "../UserDropdown/UserDropdown";
+import logo from "../../assets/logo.png";
 
 function NavigationBar({ onMenuClick }) {
   return (
-    <Navbar className="topbar" bg="danger" variant="dark" expand="lg" fixed="top">
+    <Navbar
+      className="topbar"
+      bg="danger"
+      variant="dark"
+      expand="lg"
+      fixed="top"
+    >
       <Container fluid>
         <div className="d-flex align-items-center w-100">
           {/* Botón de hamburguesa (siempre visible) */}
           <MenuToggle onMenuClick={onMenuClick} />
-          
+
           {/* Título de la aplicación */}
           <Navbar.Brand
             href="/dashboard"
             className="me-auto ms-2 text-truncate"
-            style={{ maxWidth: '200px', fontSize: '20px' }}
+            style={{ maxWidth: "200px", fontSize: "20px" }}
           >
-            Panaderia San Gabriel
+            <img
+              className="logo-login"
+              src={logo}
+              alt="Logo"
+              draggable="false"
+            />
           </Navbar.Brand>
 
           {/* Notificaciones y menú de usuario */}
