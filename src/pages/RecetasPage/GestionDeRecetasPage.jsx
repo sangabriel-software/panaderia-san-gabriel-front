7cca2515808b2ae46827b3ab37984feafb76556c
aagarcia-se <angelgarciap.dev@gmail.com>
2025-03-18 23:02:29 -0600

    ux y ui para sucursales

--- src/pages/RecetasPage/GestionDeRecetasPage.jsx
+++ src/pages/RecetasPage/GestionDeRecetasPage.jsx
@@ -1,8 +1,44 @@
+import { Container } from "react-bootstrap";
+import useGetRecetas from "../../hooks/recetas/useGetRecetas";
+import DotsMove from "../../components/Spinners/DotsMove";
+import Alert from "../../components/Alerts/Alert";
+import { BsExclamationTriangleFill } from "react-icons/bs";
 
 const GestionDeRecetasPage = () => {
-    return(
-        <h1>Hola Mundo</h1>
-    )
-}
+  const { recetas, loadingRecetas, showErrorRecetas, showInfoRecetas, setRecetas } = useGetRecetas();
+
+  console.log(recetas);
+
+  if (loadingRecetas) {
+    return (
+      <Container
+        className="d-flex justify-content-center align-items-center"
+        style={{ minHeight: "70vh" }} // Asegura que el contenedor ocupe toda la altura de la pantalla
+      >
+        <div>
+          <DotsMove />
+        </div>
+      </Container>
+    );
+  }
+
+    if (showErrorRecetas) {
+      return (
+        <Container className="justify-content-center align-items-center my-5">
+          <div className="row justify-content-center">
+            <div className="col-md-8 text-center">
+              <Alert
+                type="danger"
+                message="Hubo un error al consultar los productos y sus ingrdientes."
+                icon={<BsExclamationTriangleFill />}
+              />
+            </div>
+          </div>
+        </Container>
+      );
+    }
+
+  return <p>recetas</p>;
+};
 
 export default GestionDeRecetasPage;
