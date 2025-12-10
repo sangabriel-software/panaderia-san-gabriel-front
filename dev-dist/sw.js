b7882ce92d092b0764860421d83657ad693b2561
aagarcia-se <angelgarciap.dev@gmail.com>
2025-12-09 18:45:06 -0600

    modificar consulta de solicitud

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ti6ev5qh7vo"
+    "revision": "0.dgcmt58peig"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
