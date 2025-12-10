a0a9354cf655ececb7f42feb58539b87647a044e
aagarcia-se <angelgarciap.dev@gmail.com>
2025-12-09 18:54:02 -0600

    Config pwa

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dgcmt58peig"
+    "revision": "0.7kbii3l24t8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
