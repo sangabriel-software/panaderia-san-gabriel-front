eca7936345edffa3e561e581518c0cd0099948b1
aagarcia-se <angelgarciap.dev@gmail.com>
2025-12-11 16:02:03 -0600

    agregar mensaje de instalacion

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,21 @@
-// Service Worker mínimo válido para una PWA y compatible con TWA
+// service-worker.js - Mejor para datos que cambian frecuentemente
+
+const CACHE_NAME = 'dynamic-cache-v1';
+
 self.addEventListener("install", (event) => {
     self.skipWaiting();
-  });
-  
-  self.addEventListener("activate", (event) => {
+});
+
+self.addEventListener("activate", (event) => {
     event.waitUntil(self.clients.claim());
-  });
-  
-  self.addEventListener("fetch", () => {
-    // No hacemos nada, solo interceptamos requests
-  });
-  
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request) // 1. Intentar obtener de la red
+            .catch(() => {
+                // 2. Si la red falla (offline), intentar desde la caché
+                return caches.match(event.request);
+            })
+    );
+});
