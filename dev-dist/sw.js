fbaff65dede7b1d9efd3000cffd36ec50d60a627
aagarcia-se <angelgarciap.dev@gmail.com>
2026-01-02 23:16:04 -0600

    Agregar servicio para crecion de encuestas

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jn7rmrij4fg"
+    "revision": "0.jngutf0d6c8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
