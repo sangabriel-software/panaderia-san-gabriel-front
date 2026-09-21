a8573ef953cd2b2744892cedb84da14f930c5bd3
aagarcia <angelgarciap.dev@gmail.com>
2026-09-21 15:00:36 -0600

    Agregar filas frances

--- src/pages/Traslados/IngresarTraslado/IngresarTraslado.jsx
+++ src/pages/Traslados/IngresarTraslado/IngresarTraslado.jsx
@@ -80,14 +80,8 @@ const IngresarTraslado = () => {
         return producto.cantidadExistente;
     };
 
-    const convertirValorAUnidades = (valor, esFrances) => {
-        if (!esFrances) return Math.floor(valor);
-
-        const partes = valor.toString().split('.');
-        const filas = partes.length > 0 ? parseInt(partes[0]) : 0;
-        const unidadesExtra = partes.length > 1 ? parseInt(partes[1].substring(0, 1)) : 0;
-
-        return (filas * 6) + unidadesExtra;
+    const convertirValorAUnidades = (valor) => {
+        return Math.floor(valor);
     };
 
     const combinedStock = useMemo(() => {
@@ -235,8 +229,7 @@ const IngresarTraslado = () => {
 
             const productosCompletos = productosConStock.map(item => {
                 const producto = combinedStock.find(p => p.idProducto === item.idProducto);
-                const esFrances = producto.nombreProducto === "Frances";
-                const cantidadATrasladar = convertirValorAUnidades(item.value, esFrances);
+                const cantidadATrasladar = convertirValorAUnidades(item.value);
 
                 return {
                     ...producto,
